69212aacd31e615997dc525aaaa212215a33041a
vitorfelipep <vitorfelipep@gmail.com>
2016-10-10 21:20:34 -0300

    Cadastro cerveja com upload de foto

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,6 +19,7 @@ import com.algaworks.brewer.repository.Estilos;
 import com.algaworks.brewer.service.CadastroCervejaService;
 
 @Controller
+@RequestMapping("/cervejas")
 public class CervejasController {
 	
 	@Autowired
@@ -26,7 +28,7 @@ public class CervejasController {
 	@Autowired
 	private CadastroCervejaService cadastroCervejaService;
 		
-	@RequestMapping("/cervejas/novo")
+	@RequestMapping("/novo")
 	public ModelAndView novo(Cerveja cerveja) {
 		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
 		mv.addObject("sabores", Sabor.values());
@@ -35,7 +37,7 @@ public class CervejasController {
 		return mv;
 	}
 	
-	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
+	@RequestMapping(value = "/novo", method = RequestMethod.POST)
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		
 		if (result.hasErrors()) {
@@ -48,5 +50,10 @@ public class CervejasController {
 		return new ModelAndView("redirect:/cervejas/novo");
 	}
 	
-	
+	@GetMapping
+	public ModelAndView pesquisar() {
+		ModelAndView mv = new ModelAndView("cerveja/pesquisaCervejas");
+		
+		return mv;
+	}
 }
--- src/main/resources/static/javascripts/cerveja.upload-foto.js
+++ src/main/resources/static/javascripts/cerveja.upload-foto.js
@@ -18,14 +18,36 @@ Brewer.UploadFoto = (function() {
 				type: 'json',
 				filelimit: 1,
 				allow: '*.(jpg|jpeg|png)',
-				action: '/brewer/fotos',
-				complete: function(resposta) {
-					
-				}
+				action: this.containerFotoCerveja.data('url-fotos'),
+				complete: onUploadCompleto.bind(this)
 		}
 		
 		UIkit.uploadSelect($('#upload-select'), settings);
 		UIkit.uploadDrop(this.uploadDrop, settings);
+		
+		if(this.inputNomeFoto.val()) {
+			//Força o contexto this a trabalhar com os valores já existentes no DOM
+			onUploadCompleto.call(this, {nome: this.inputNomeFoto.val(), contentType: this.inputContentType.val()});
+		}
+		
+	}
+	
+	function onUploadCompleto(resposta) {
+		this.inputNomeFoto.val(resposta.nome);
+		this.inputContentType.val(resposta.contentType);
+		
+		this.uploadDrop.addClass('hidden');
+		var htmlFotoCerveja = this.template({nomeFoto: resposta.nome});
+		this.containerFotoCerveja.append(htmlFotoCerveja);
+		
+		$('.js-remove-foto').on('click', onRemoverFoto.bind(this));
+	}
+	
+	function onRemoverFoto() {
+		$('.js-foto-cerveja').remove();
+		this.uploadDrop.removeClass('hidden');
+		this.inputNomeFoto.val('');
+		this.inputContentType.val('');
 	}
 	
 	return UploadFoto;
